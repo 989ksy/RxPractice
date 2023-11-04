2992964fc069e593cf713cb74690198b766837e4
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-04 16:58:21 +0900

    [Add] ShoppingListButton 스토리보드 추가

--- SwiftRxPractice/SwiftRxPractice/ViewController.swift
+++ SwiftRxPractice/SwiftRxPractice/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var ShoppingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
 
-
+    @IBAction func shoppingButtonTapped(_ sender: Any) {
+        
+        let vc = ShoppingListViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
+
+
+
 
