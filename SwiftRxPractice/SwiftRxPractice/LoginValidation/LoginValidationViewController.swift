00d1a43f4ef4a069871679d9a0a159589ebbe69f
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-13 23:14:33 +0900

    [Practice] LoginValidation Rx

---
+++ SwiftRxPractice/SwiftRxPractice/LoginValidation/LoginValidationViewController.swift
@@ -0,0 +1,131 @@
+//
+//  LoginValidationViewController.swift
+//  SwiftRxPractice
+//
+//  Created by Seungyeon Kim on 11/13/23.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class LoginValidationViewController : UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        configure()
+        bind()
+        
+    }
+    
+    
+    
+    //MARK: - bind
+    
+    func bind() {
+        
+        let login = loginTextfield.rx.text.orEmpty
+        let password = passwordTextfield.rx.text.orEmpty
+        
+        let validation = Observable.combineLatest(login, password) { login, password in
+            return login.count >= 6 && password.count >= 8
+        }
+        
+        validation
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        validation
+            .subscribe(with: self) { owner, value in
+                let color = UIColor.green
+                owner.nextButton.backgroundColor = value ? color : UIColor.red
+                owner.loginTextfield.backgroundColor = value ? UIColor.white : UIColor.gray
+                owner.passwordTextfield.backgroundColor = value ? UIColor.white : UIColor.gray
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .subscribe(with: self) { owner, value in
+                print("tapped")
+            }
+            .disposed(by: disposeBag)
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    //MARK: - Configure UI
+        
+        let loginTextfield = {
+            
+            let view = UITextField()
+            view.placeholder = "6글자 이상의 아이디를 입력하세요."
+            view.borderStyle = .line
+            view.textColor = .black
+            view.layer.cornerRadius = 5
+            view.layer.borderWidth = 1
+            view.clipsToBounds = true
+            return view
+            
+        }()
+        
+        let passwordTextfield = {
+            let view = UITextField()
+            view.placeholder = "8글자 이상의 비밀번호를 입력하세요."
+            view.borderStyle = .line
+            view.textColor = .black
+            view.layer.cornerRadius = 5
+            view.layer.borderWidth = 1
+            view.clipsToBounds = true
+            return view
+        }()
+        
+        let nextButton = {
+            let view = UIButton()
+            view.setTitle("로그인", for: .normal)
+            view.backgroundColor = .brown
+            view.layer.cornerRadius = 20
+            return view
+        }()
+        
+    func configure() {
+        
+        view.addSubview(loginTextfield)
+        view.addSubview(passwordTextfield)
+        view.addSubview(nextButton)
+        
+        loginTextfield.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(120)
+            $0.horizontalEdges.equalToSuperview().inset(50)
+            $0.height.equalTo(30)
+        }
+        
+        passwordTextfield.snp.makeConstraints {
+            $0.top.equalTo(loginTextfield.snp.bottom).offset(15)
+            $0.horizontalEdges.equalToSuperview().inset(50)
+            $0.height.equalTo(30)
+        }
+        nextButton.snp.makeConstraints{
+            $0.top.equalTo(passwordTextfield.snp.bottom).offset(15)
+            $0.horizontalEdges.equalToSuperview().inset(50)
+            $0.height.equalTo(50)
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
